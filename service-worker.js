1865beb6d41a6debe5b6dafbc8b7f7d82d08b102
u28ozjhp <113915920+u28ozjhp@users.noreply.github.com>
2023-07-18 23:37:48 +0900

    a

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,33 @@
-const a = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m = [
-  a + "/app/immutable/entry/app.f93d0c53.js",
-  a + "/app/immutable/assets/0.45520334.css",
-  a + "/app/immutable/nodes/0.6e473da6.js",
-  a + "/app/immutable/nodes/1.d7e3d94c.js",
-  a + "/app/immutable/assets/2.971532cb.css",
-  a + "/app/immutable/nodes/2.c4b376f9.js",
-  a + "/app/immutable/assets/3.fd2e7e0b.css",
-  a + "/app/immutable/nodes/3.fc745d8a.js",
-  a + "/app/immutable/nodes/4.dc47b7bb.js",
-  a + "/app/immutable/nodes/5.6166a206.js",
-  a + "/app/immutable/assets/Spinner.d9b647a8.css",
-  a + "/app/immutable/chunks/Spinner.83fce099.js",
-  a + "/app/immutable/chunks/each.1a80d1c6.js",
-  a + "/app/immutable/chunks/index.bbd4559d.js",
-  a + "/app/immutable/chunks/index.c77a4116.js",
-  a + "/app/immutable/chunks/scheduler.a1dc3b87.js",
-  a + "/app/immutable/chunks/singletons.3b26fd86.js",
-  a + "/app/immutable/chunks/store.f1b5e2f5.js",
-  a + "/app/immutable/entry/start.659b9aac.js"
+const e = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m = [
+  e + "/app/immutable/entry/app.ae0cf021.js",
+  e + "/app/immutable/assets/0.1eaab022.css",
+  e + "/app/immutable/nodes/0.6e17cf53.js",
+  e + "/app/immutable/nodes/1.5c71e13f.js",
+  e + "/app/immutable/assets/2.971532cb.css",
+  e + "/app/immutable/nodes/2.15c8323f.js",
+  e + "/app/immutable/assets/3.fd2e7e0b.css",
+  e + "/app/immutable/nodes/3.4ad5d2ce.js",
+  e + "/app/immutable/nodes/4.78901f05.js",
+  e + "/app/immutable/nodes/5.d653e0d4.js",
+  e + "/app/immutable/assets/Spinner.d9b647a8.css",
+  e + "/app/immutable/chunks/Spinner.c154e812.js",
+  e + "/app/immutable/chunks/each.d490452d.js",
+  e + "/app/immutable/chunks/index.c79ec7ab.js",
+  e + "/app/immutable/chunks/index.f0a0f8d2.js",
+  e + "/app/immutable/chunks/scheduler.f620a928.js",
+  e + "/app/immutable/chunks/singletons.6bdb5760.js",
+  e + "/app/immutable/chunks/store.83c04e3c.js",
+  e + "/app/immutable/chunks/stores.e6061ecb.js",
+  e + "/app/immutable/entry/start.4f418e18.js"
 ], l = [
-  a + "/kimigayo.mp3",
-  a + "/kimigayo.opus",
-  a + "/logo.svg",
-  a + "/manifest.json",
-  a + "/pwa-assets/apple-icon-180.png",
-  a + "/pwa-assets/manifest-icon-192.maskable.png",
-  a + "/pwa-assets/manifest-icon-512.maskable.png"
-], u = "1689687358308", i = `cache-${u}`, p = [
+  e + "/kimigayo.mp3",
+  e + "/kimigayo.opus",
+  e + "/logo.svg",
+  e + "/manifest.json",
+  e + "/pwa-assets/apple-icon-180.png",
+  e + "/pwa-assets/manifest-icon-192.maskable.png",
+  e + "/pwa-assets/manifest-icon-512.maskable.png"
+], u = "1689690709693", i = `cache-${u}`, p = [
   ...m,
   ...l
 ];
@@ -38,8 +39,8 @@ self.addEventListener("install", (s) => {
 });
 self.addEventListener("activate", (s) => {
   async function t() {
-    for (const e of await caches.keys())
-      e !== i && await caches.delete(e);
+    for (const a of await caches.keys())
+      a !== i && await caches.delete(a);
   }
   s.waitUntil(t());
 });
@@ -47,9 +48,9 @@ self.addEventListener("fetch", (s) => {
   if (s.request.method !== "GET")
     return;
   async function t() {
-    const e = new URL(s.request.url), n = await caches.open(i);
-    if (p.includes(e.pathname))
-      return n.match(e.pathname);
+    const a = new URL(s.request.url), n = await caches.open(i);
+    if (p.includes(a.pathname))
+      return n.match(a.pathname);
     try {
       const c = await fetch(s.request);
       return c.status === 200 && n.put(s.request, c.clone()), c;
