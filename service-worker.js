e5e1eb93767dd6f48bf787a43fe0328fd937c411
u28ozjhp <113915920+u28ozjhp@users.noreply.github.com>
2023-07-19 23:40:09 +0900

    a

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,25 @@
 const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m = [
-  s + "/app/immutable/entry/app.94d583f5.js",
-  s + "/app/immutable/assets/0.a85688af.css",
-  s + "/app/immutable/nodes/0.8122987f.js",
-  s + "/app/immutable/nodes/1.0df84d4f.js",
+  s + "/app/immutable/entry/app.e97b9924.js",
+  s + "/app/immutable/assets/0.f402590a.css",
+  s + "/app/immutable/nodes/0.b83d0a74.js",
+  s + "/app/immutable/nodes/1.890f8c9e.js",
   s + "/app/immutable/assets/2.971532cb.css",
   s + "/app/immutable/nodes/2.6c63400b.js",
   s + "/app/immutable/assets/3.fd2e7e0b.css",
   s + "/app/immutable/nodes/3.91a943f2.js",
   s + "/app/immutable/nodes/4.7e58ec11.js",
-  s + "/app/immutable/assets/5.1d709fce.css",
-  s + "/app/immutable/nodes/5.141971cb.js",
+  s + "/app/immutable/assets/5.dba156e9.css",
+  s + "/app/immutable/nodes/5.fa15f659.js",
   s + "/app/immutable/assets/Spinner.d9b647a8.css",
   s + "/app/immutable/chunks/Spinner.854c52f8.js",
   s + "/app/immutable/chunks/each.7271e79e.js",
   s + "/app/immutable/chunks/index.506cce27.js",
   s + "/app/immutable/chunks/index.5fdc0ad9.js",
   s + "/app/immutable/chunks/scheduler.212c71d7.js",
-  s + "/app/immutable/chunks/singletons.88b78124.js",
+  s + "/app/immutable/chunks/singletons.57a3967e.js",
   s + "/app/immutable/chunks/store.a425e42c.js",
-  s + "/app/immutable/chunks/stores.1f22e438.js",
-  s + "/app/immutable/entry/start.35393399.js"
+  s + "/app/immutable/chunks/stores.3fcd5686.js",
+  s + "/app/immutable/entry/start.c3777910.js"
 ], l = [
   s + "/kimigayo.mp3",
   s + "/kimigayo.opus",
@@ -28,7 +28,7 @@ const s = /* @__PURE__ */ location.pathname.split("/").slice(0, -1).join("/"), m
   s + "/pwa-assets/apple-icon-180.png",
   s + "/pwa-assets/manifest-icon-192.maskable.png",
   s + "/pwa-assets/manifest-icon-512.maskable.png"
-], u = "1689771329919", i = `cache-${u}`, p = [
+], u = "1689777437913", i = `cache-${u}`, p = [
   ...m,
   ...l
 ];
@@ -49,14 +49,14 @@ self.addEventListener("fetch", (a) => {
   if (a.request.method !== "GET")
     return;
   async function t() {
-    const e = new URL(a.request.url), n = await caches.open(i);
+    const e = new URL(a.request.url), c = await caches.open(i);
     if (p.includes(e.pathname))
-      return n.match(e.pathname);
+      return c.match(e.pathname);
     try {
-      const c = await fetch(a.request);
-      return c.status === 200 && n.put(a.request, c.clone()), c;
+      const n = await fetch(a.request);
+      return n.status === 200 && c.put(a.request, n.clone()), n;
     } catch {
-      return n.match(a.request);
+      return c.match(a.request);
     }
   }
   a.respondWith(t());
